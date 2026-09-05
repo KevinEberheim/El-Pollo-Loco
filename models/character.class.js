c854c27e7fa6f5f6a932f0eba151c28406a3eacd
KevinEberheim <kevin.eberheim@gmail.com>
2026-09-05 15:19:29 +0200

    -add sleep animation for charakter
    -add a reflection of charakter in other direction

--- models/character.class.js
+++ models/character.class.js
@@ -11,8 +11,19 @@ class Character extends MovableObject {
         '../img/2_character_pepe/1_idle/idle/I-7.png',
         '../img/2_character_pepe/1_idle/idle/I-8.png',
         '../img/2_character_pepe/1_idle/idle/I-9.png',
-        '../img/2_character_pepe/1_idle/idle/I-10.png'
+        '../img/2_character_pepe/1_idle/idle/I-10.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-11.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-12.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-13.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-14.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-15.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-16.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-17.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-18.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-19.png',
+        '../img/2_character_pepe/1_idle/long_idle/I-20.png'
     ];
+
     IMAGES_WALKING = [
         '../img/2_character_pepe/2_walk/W-21.png',
         '../img/2_character_pepe/2_walk/W-22.png',
@@ -41,10 +52,13 @@ class Character extends MovableObject {
         setInterval(() => {
             if (this.world.keyboard.LEFT) {
                 this.x -= 5;
+                this.otherDirection = true;
             }
             if (this.world.keyboard.RIGHT) {
                 this.moveRight();
+                this.otherDirection = false;
             }
+            this.world.camera_x = -this.x + 100;
         }, 1000 / 10);
 
         setInterval(() => {
--- models/world.class.js
+++ models/world.class.js
@@ -16,6 +16,8 @@ class World {
     ];
     ctx;
     keyboard;
+    camera_x = 0;
+
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
         this.keyboard = keyboard;
@@ -29,12 +31,12 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-
+        this.ctx.translate(this.camera_x, 0);
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
-        
+        this.ctx.translate(-this.camera_x, 0);
 
         requestAnimationFrame(() => this.draw());
     }
@@ -42,9 +44,20 @@ class World {
     addObjectsToMap(objects) {
         objects.forEach(object => {
             this.addToMap(object);
-        });}
+        });
+    }
 
     addToMap(movableObject) {
+        if(movableObject.otherDirection) {
+            this.ctx.save();
+            this.ctx.translate(movableObject.width, 0);
+            this.ctx.scale(-1, 1);
+            movableObject.x = movableObject.x * -1;
+        }
         this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+        if(movableObject.otherDirection) {
+            movableObject.x = movableObject.x * -1;
+            this.ctx.restore();
+        }
     }
 }
